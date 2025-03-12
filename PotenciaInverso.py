ca2e241697d96db6292663912026c752890fd4f7
Mariana2602 <lopezmariana2602@gmail.com>
2025-03-12 14:26:03 -0400

    Update PotenciaInverso.py

--- PotenciaInverso.py
+++ PotenciaInverso.py
@@ -13,10 +13,15 @@ class PotenciaInversa:
         self.columnas = len(matriz[0])
         self.matriz_inversa = self.inversa()
 
+    def matriz_vector(self, matriz, vector):
+        return [sum(matriz[i][j] * vector[j] for j in range(len(vector))) for i in range(len(matriz))]
+
+    def norma(self, vector):
+        return sum(i**2 for i in vector) ** 0.5
+    
     def inversa(self):
         matriz = [i[:] for i in self.matriz]
         identidad = [[1 if i == j else 0 for j in range(self.columnas)] for i in range(self.filas)]
-
         if self.filas != self.columnas:
             return None
 
@@ -24,7 +29,6 @@ class PotenciaInversa:
             pivote = matriz[i][i]
             if pivote == 0:
                 return None
-
             for j in range(self.columnas):
                 matriz[i][j] /= pivote
                 identidad[i][j] /= pivote
@@ -38,46 +42,53 @@ class PotenciaInversa:
 
         return identidad
 
-    def matriz_vector(self, matriz, vector):
-        return [sum(matriz[i][j] * vector[j] for j in range(len(vector))) for i in range(len(matriz))]
-
-    def norma(self, vector):
-        return sum(i**2 for i in vector) ** 0.5
-
     def potencia(self):
         n = len(self.matriz)
         vector = [random.uniform(0, 1) for i in range(n)]
-        norma_vector = self.norma(vector)
-        vector = [x / norma_vector for x in vector]
+        matriz_inversa = self.inversa()
+        if matriz_inversa is None:
+            print("La matriz no tiene inversa")
+            return None, None
 
-        autovalores = []
+        autovalor_anterior = 0
+        autovalor_convergencia = []
+        autovector_convergencia = []
 
-        for _ in range(self.iteraciones):
-            nuevo_vector = self.matriz_vector(self.matriz_inversa, vector) 
-            lambda_inverso = sum(vector[i] * nuevo_vector[i] for i in range(n))
-            autovalor = 1 / lambda_inverso  
-            autovalores.append(autovalor)
-            
-            norma_nuevo = self.norma(nuevo_vector)
-            nuevo_vector = [x / norma_nuevo for x in nuevo_vector]
-            
-            if self.norma([nuevo_vector[i] - vector[i] for i in range(n)]) < self.tolerancia:
+        for i in range(self.iteraciones):
+            nuevo_vector = self.matriz_vector(matriz_inversa, vector)
+            norma = self.norma(nuevo_vector)
+            vector = [x / norma for x in nuevo_vector]
+            autovalor = sum(vector[i] * nuevo_vector[i] for i in range(n))
+            autovalor_convergencia.append(autovalor)
+            autovector_convergencia.append(list(vector))
+            if abs(autovalor - autovalor_anterior) < self.tolerancia:
                 break
-            
-            vector = nuevo_vector
 
-        return autovalores
+            autovalor_anterior = autovalor
+
+        return autovalor_convergencia, autovector_convergencia
 
 matriz = [[random.randint(1, 9) for _ in range(2)] for _ in range(2)]
-matriz = [[(matriz[i][j] + matriz[j][i]) // 2 for j in range(2)] for i in range(2)]
-
 potencia = PotenciaInversa(matriz)
-autovalores = potencia.potencia()
+autovalor_convergencia, autovector_convergencia = potencia.potencia()
 
-plt.figure(figsize=(6, 6))
-plt.plot(range(1, len(autovalores) + 1), autovalores, marker='o', linestyle='-', color='r')
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.plot(range(1, len(autovalor_convergencia) + 1), autovalor_convergencia, marker='o', linestyle='-')
 plt.xlabel("Iteraciones")
-plt.ylabel("Autovalor más pequeño")
-plt.title("Convergencia del Autovalor más Pequeño")
+plt.ylabel("Autovalor dominante")
+plt.title("Convergencia del Autovalor")
 plt.grid()
+
+plt.subplot(1, 2, 2)
+for i in range(len(autovector_convergencia[0])): 
+    plt.plot(range(1, len(autovector_convergencia) + 1), autovector_convergencia, marker='o', linestyle='-', label=f'Componente {i+1}')
+plt.xlabel("Iteraciones")
+plt.ylabel("Valor de la Componente")
+plt.title("Convergencia del Autovector")
+plt.legend()
+plt.grid()
+
+plt.tight_layout()
 plt.show()
